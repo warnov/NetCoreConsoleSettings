fd11f0ae9eb249d96f9880d647007fd5a3fb7a2e
warnov <warnov@gmail.com>
2017-01-15 22:54:26 -0500

    Solution ready to show

---
+++ src/NetCoreConsoleSettings/ConfigHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace NetCoreConsoleSettings
+{
+    public static class ConfigHelper
+    {
+        public static IConfiguration Config
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                return builder.Build();
+            }
+        }
+    }
+}
---
+++ src/NetCoreConsoleSettings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace NetCoreConsoleSettings
+{
+    public class Program
+    {
+        static IConfiguration _config = ConfigHelper.Config;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"The value for setting1 is {_config["setting1"]} and for setting2 is {_config["setting2"]}");
+            Console.ReadLine();
+        }
+    }
+}
