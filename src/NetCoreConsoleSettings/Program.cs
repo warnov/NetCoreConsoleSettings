993ae60eab290bc45618d83068f2034c49e3df86
warnov <warnov@UNDERBEAST>
2017-01-16 23:17:12 -0500

    ConfigHelper included in Program.cs as a static using to give more clarity to the code.

--- src/NetCoreConsoleSettings/Program.cs
+++ src/NetCoreConsoleSettings/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+using static NetCoreConsoleSettings.ConfigHelper;
 
 namespace NetCoreConsoleSettings
 {
     public class Program
-    {
-        static IConfiguration _config = ConfigHelper.Config;
+    {       
         public static void Main(string[] args)
         {
-            Console.WriteLine($"The value for setting1 is {_config["setting1"]} and for setting2 is {_config["setting2"]}");
+            Console.WriteLine($"The value for setting1 is {Config["setting1"]} and for setting2 is {Config["setting2"]}");
             Console.ReadLine();
         }
     }
